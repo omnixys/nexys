5048541a920e2be56b59512d47b3753e05014c40
Caleb-Script <caleb-script@outlook.de>
2026-03-02 20:26:15 +0100

    feat(versioning): add semantic versioning and package

---
+++ src/app/index.ts
@@ -0,0 +1,5 @@
+export { default as NexysHome } from "./home/page";
+export { default as NexysProfile } from "./profile/page";
+export { default as NexysSecurity } from "./security/page";
+export { default as NexysSettings } from "./settings/page";
+export { default as NexysSupport } from "./support/page";
--- src/components/background/StarBackground.tsx
+++ src/components/background/StarBackground.tsx
@@ -7,22 +7,32 @@ import { Box } from "@mui/material";
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
+const STAR_COUNT = 5000;
+
 const StarBackground = (props: any) => {
   const ref = useRef<any>(null);
 
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 }),
-  );
+  const positions = React.useMemo(() => {
+    const array = new Float32Array(STAR_COUNT * 3);
+    random.inSphere(array, { radius: 1.2 });
+    return array;
+  }, []);
 
   useFrame((_, delta) => {
     if (!ref.current) return;
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
+    ref.current.rotation.x -= delta * 0.1;
+    ref.current.rotation.y -= delta * 0.07;
   });
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points
+        ref={ref}
+        positions={positions}
+        stride={3}
+        frustumCulled
+        {...props}
+      >
         <PointMaterial
           transparent
           color="#ffffff"
